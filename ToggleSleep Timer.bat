f23f895d50e05acad5643b94c4d39c555fe4abd6
NazmulHaque01 <89252855+NazmulHaque01@users.noreply.github.com>
2025-10-18 18:50:22 +0600

    Add files via upload

---
+++ ToggleSleep Timer.bat
@@ -0,0 +1,34 @@
+@echo off
+setlocal EnableDelayedExpansion
+
+:loop
+cls
+echo ================================
+echo Current Active Power Plan:
+for /f "tokens=*" %%i in ('powercfg /getactivescheme') do (
+    echo %%i
+    set "current=%%i"
+)
+echo ================================
+echo Press [T] then [Enter] to toggle
+echo Press any other key to exit
+set /p choice=Your input: 
+
+if /i "!choice!"=="t" (
+    echo.
+    echo Toggling power plan...
+    echo.
+
+    echo !current! | findstr /i "SleepAfter10" >nul
+    if !errorlevel! == 0 (
+        powercfg /setactive ......Power Plan's GUID......
+    ) else (
+        powercfg /setactive ......Power Plan's GUID......
+    )
+    timeout /t 2 >nul
+    goto loop
+) else (
+    echo Exiting...
+    timeout /t 1 >nul
+    exit /b
+)
